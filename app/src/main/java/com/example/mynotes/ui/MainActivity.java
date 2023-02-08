9f0ae66194197d4376f6572741b5067a64f1c599
Romajke <skuaall@mail.ru>
2023-02-08 15:39:16 +0300

    добавил боковое меню

--- app/src/main/java/com/example/mynotes/ui/MainActivity.java
+++ app/src/main/java/com/example/mynotes/ui/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.mynotes.ui;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.mynotes.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,15 +24,62 @@ public class MainActivity extends AppCompatActivity {
 
         initToolBar();
 
+        if (!isLandscape())
+            initToolbarAndDrawer();
+        else
+            initToolBar();
+
         if (savedInstanceState == null) getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.notes_container, new NotesFragment())
                 .commit();
     }
 
-    private void initToolBar() {
+    private void initToolbarAndDrawer() {
+        Toolbar toolbar = initToolBar();
+        initDrawer(toolbar);
+    }
+
+    private void initDrawer(Toolbar toolbar) {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.about_action:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .addToBackStack("")
+                                .add(R.id.notes_container, new AboutProgramFragment())
+                                .commit();
+                        return true;
+                    case R.id.exit_action:
+                        finish();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+    }
+
+    private boolean isLandscape() {
+        return getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+    }
+
+    private Toolbar initToolBar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        return toolbar;
     }
 
     @Override
